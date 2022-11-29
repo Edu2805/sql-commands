431fdfc74d21b02601e7bc10579da0731bed102f
Eduardo <eduardo@MacBook-Air-de-Eduardo.local>
2022-11-29 08:16:04 -0300

    Agrupadores e consultas

--- comandos.sql
+++ comandos.sql
@@ -138,6 +138,8 @@ create table alunos (
 
 insert into alunos (nome) values ('Carlos');
 insert into alunos (nome) values ('JOsé');
+insert into alunos (nome) values ('Maria');
+insert into alunos (nome) values ('Joana');
 
 select * from alunos
 
@@ -148,6 +150,8 @@ create table cursos (
 
 insert into cursos (nome) values ('Java');
 insert into cursos (nome) values ('TypeScript');
+insert into cursos (nome) values ('CSharp');
+insert into cursos (nome) values ('C++');
 
 select * from cursos
 
@@ -159,6 +163,7 @@ create table alunos_cursos (
 )
 
 insert into alunos_cursos (aluno_id, curso_id) values (1,1);
+insert into alunos_cursos (aluno_id, curso_id) values (2,1);
 insert into alunos_cursos (aluno_id, curso_id) values (2,1);
 
 select * from alunos_cursos 
@@ -229,3 +234,227 @@ select alunos.nome as "Nome do aluno",
 	   cursos.nome as "Nome do curso"
 from alunos
 cross join cursos
+
+--- Cascade delete
+drop table alunos_cursos 
+
+create table alunos_cursos (
+	aluno_id INTEGER,
+	curso_id INTEGER,
+	primary key (aluno_id, curso_id),
+	foreign key (aluno_id) references alunos (id)
+	on delete cascade,
+	foreign key (curso_id) references cursos (id)
+)
+
+insert into alunos_cursos (aluno_id, curso_id) values (1,1);
+insert into alunos_cursos (aluno_id, curso_id) values (1,2);
+insert into alunos_cursos (aluno_id, curso_id) values (2,1);
+insert into alunos_cursos (aluno_id, curso_id) values (2,2);
+
+delete from alunos where id = 1;
+
+select alunos.nome as "Nome do aluno",
+	   cursos.nome as "Nome do curso"
+from alunos 
+join alunos_cursos 
+on alunos_cursos.aluno_id = alunos.id
+join cursos 
+on cursos.id = alunos_cursos.curso_id
+
+--- Cascade update
+drop table alunos_cursos 
+
+create table alunos_cursos (
+	aluno_id INTEGER,
+	curso_id INTEGER,
+	primary key (aluno_id, curso_id),
+	foreign key (aluno_id) references alunos (id)
+	on delete cascade
+	on update cascade,
+	foreign key (curso_id) references cursos (id)
+)
+
+insert into alunos_cursos (aluno_id, curso_id) values (1,1);
+insert into alunos_cursos (aluno_id, curso_id) values (1,2);
+insert into alunos_cursos (aluno_id, curso_id) values (2,1);
+insert into alunos_cursos (aluno_id, curso_id) values (2,2);
+insert into alunos_cursos (aluno_id, curso_id) values (3,4);
+insert into alunos_cursos (aluno_id, curso_id) values (4,4);
+
+delete from alunos where id = 1;
+
+select alunos.id as "Id do aluno",
+	   alunos.nome as "Nome do aluno",
+	   cursos.id as "Id do curso",
+	   cursos.nome as "Nome do curso"
+from alunos 
+join alunos_cursos 
+on alunos_cursos.aluno_id = alunos.id
+join cursos 
+on cursos.id = alunos_cursos.curso_id
+
+update alunos set id = 10
+where id = 2
+
+--- Consultas
+drop table funcionarios 
+
+create table funcionarios (
+	id serial primary key,
+	matricula varchar(10),
+	nome varchar(100),
+	sobrenome varchar(100)
+	
+);
+
+insert into funcionarios (matricula, nome, sobrenome) values('M001', 'Diego', 'Nogueira');
+insert into funcionarios (matricula, nome, sobrenome) values('M002', 'Marcela', 'Faria');
+insert into funcionarios (matricula, nome, sobrenome) values('M003', 'Ricardo', 'Amorim');
+insert into funcionarios (matricula, nome, sobrenome) values('M004', 'Maria', 'Mathias');
+insert into funcionarios (matricula, nome, sobrenome) values('M005', 'Graziele', 'Amorim');
+insert into funcionarios (matricula, nome, sobrenome) values('M006', 'Luiz', 'Roberto');
+
+-- Ascendente por padrão
+select * from funcionarios 
+order by nome
+
+-- Descendente
+select * from funcionarios 
+order by nome desc
+
+-- Descendente
+select * from funcionarios 
+order by nome asc 
+
+-- Ordenação com duas colunas
+select * from funcionarios 
+order by nome, sobrenome asc 
+
+-- Ordenação pela posição do campo (4 - sobrenome)
+select * from funcionarios 
+order by 4
+
+-- Ordenação por várias posições
+select * from funcionarios 
+order by 3, 4, 2
+
+-- Ordenação específica
+select * from funcionarios 
+order by 4 desc, nome desc, 2
+
+-- Evitando referência ambígua
+select alunos.id as "Id do aluno",
+	   alunos.nome as "Nome do aluno",
+	   cursos.id as "Id do curso",
+	   cursos.nome as "Nome do curso"
+from alunos 
+join alunos_cursos 
+on alunos_cursos.aluno_id = alunos.id
+join cursos 
+on cursos.id = alunos_cursos.curso_id
+order by alunos.nome desc, cursos.id 
+
+--- Limite de dados
+select * from funcionarios limit 3
+
+-- ordenado
+select * from funcionarios 
+order by nome
+limit 3
+
+-- Paginação (trazendo desde o segundo registro limitado a 3)
+select * from funcionarios 
+order by nome
+limit 3
+offset 1
+
+-- Paginação (trazendo desde o primeiro registro limitado a 3)
+select * from funcionarios 
+order by nome
+limit 3
+offset 0
+
+--- Funções de agregação
+-- Count 
+select count(*) from funcionarios  
+
+-- Count especificando o campo
+select count(id) from funcionarios 
+
+-- SUM Somando os ids
+select count(id), 
+sum(id)
+from funcionarios 
+
+-- MAX retornar o maior valor da coluna id
+select count(id), 
+sum(id),
+max(id)
+from funcionarios 
+
+-- MIN retornar o menor valor da coluna id
+select count(id), 
+sum(id),
+max(id),
+min(id)
+from funcionarios 
+
+-- AVG retornar a média do valor da coluna id
+select count(id), 
+sum(id),
+max(id),
+min(id),
+round(avg(id), 2)
+from funcionarios 
+
+--- Agrupamento de consultas
+-- Evitar repetição de dados
+select distinct sobrenome
+from funcionarios 
+order by sobrenome
+
+-- Quando precisa de uma funcao de agregação (usar o group by)
+select nome, sobrenome, count(id)
+from funcionarios
+group by nome, sobrenome
+order by sobrenome
+
+-- Indicando a posição da coluna na tabela
+select nome, sobrenome, count(id)
+from funcionarios
+group by 1, 2
+order by sobrenome
+
+--- Filtros em consultas agrupadas
+-- Saber se tem alunos matriculados
+select * from cursos
+left join alunos_cursos
+on alunos_cursos.aluno_id = cursos.id 
+left join alunos on alunos.id = alunos_cursos.aluno_id
+
+-- Quantos alunos matriculados em cada curso
+select cursos.nome, count(alunos.id) 
+from cursos
+left join alunos_cursos
+on alunos_cursos.aluno_id = cursos.id 
+left join alunos on alunos.id = alunos_cursos.aluno_id
+group by 1
+
+-- Usando o having cursos sem alunos
+select cursos.nome, count(alunos.id) 
+from cursos
+left join alunos_cursos
+on alunos_cursos.aluno_id = cursos.id 
+left join alunos on alunos.id = alunos_cursos.aluno_id
+group by 1
+having count (alunos.id) = 0
+
+-- Usando o having cursos com alunos
+select cursos.nome, count(alunos.id) 
+from cursos
+left join alunos_cursos
+on alunos_cursos.aluno_id = cursos.id 
+left join alunos on alunos.id = alunos_cursos.aluno_id
+group by 1
+having count (alunos.id) > 0
