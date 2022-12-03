a36ef2d27b76b15e1a5c8ba617ff27238bb93b9f
Eduardo <eduardo@MacBook-Air-de-Eduardo.local>
2022-12-03 17:58:35 -0300

    Aprimoramento em relatórios

--- banco-inicial.sql
+++ banco-inicial.sql
@@ -25,6 +25,8 @@ CREATE TABLE aluno_curso (
 );
 
 
+-----------------------------------------------------------------------
+
 insert into aluno (primeiro_nome, ultimo_nome, data_nascimento) 
 values ('Carlos', 'Alberto', '1980-10-11'),
 ('José', 'Ricardo', '1985-02-15'),
@@ -47,9 +49,104 @@ values ('Java', 4), ('TypeScript', 5), ('GitLab', 2),
 
 select * from curso 
 
-insert into aluno_curso (aluno_id, curso_id) 
-values (1,5), (5, 4), 
-(2, 2), (3, 5), (4, 3);
+insert into aluno_curso values (1,5), (5, 4), (2, 2), (3, 5), (4, 3);
 
 select * from aluno_curso 
 
+-- Update
+
+select * from categoria c 
+
+update categoria set nome = 'Ciência de dados' where id = 3
+
+--- Relatório de alunos por curso
+
+-- Geral
+select * from aluno a 
+join aluno_curso ac on ac.aluno_id = a.id 
+join curso c on c.id = ac.curso_id 
+
+-- Específico informando as colunas no group by
+select a.primeiro_nome as "Primeiro nome", 
+	   a.ultimo_nome as "Segundo nome",
+	   count (c.id) as "Número de cursos"
+from aluno a 
+join aluno_curso ac on ac.aluno_id = a.id 
+join curso c on c.id = ac.curso_id 
+group by a.primeiro_nome, a.ultimo_nome 
+
+-- Específico informando as posições das colunas no group by
+select a.primeiro_nome as "Primeiro nome", 
+	   a.ultimo_nome as "Segundo nome",
+	   count (c.id) as "Número de cursos"
+from aluno a 
+join aluno_curso ac on ac.aluno_id = a.id 
+join curso c on c.id = ac.curso_id 
+group by 1,2 
+
+-- Ordenado pelo pelo alias (numero de cursos)
+select a.primeiro_nome as "Primeiro nome", 
+	   a.ultimo_nome as "Segundo nome",
+	   count (c.id) as "Número de cursos"
+from aluno a 
+join aluno_curso ac on ac.aluno_id = a.id 
+join curso c on c.id = ac.curso_id 
+group by 1,2 
+order by "Número de cursos" desc  
+
+-- Limpando a query, deixando somente os dados necessários sem aterar o resultado
+select a.primeiro_nome as "Primeiro nome", 
+	   a.ultimo_nome as "Segundo nome",
+	   count (ac.curso_id) as "Número de cursos"
+from aluno a 
+join aluno_curso ac on ac.aluno_id = a.id 
+group by 1,2 
+order by "Número de cursos" desc  
+
+-- Limitando pelo aluno que fez mais cursos
+select a.primeiro_nome as "Primeiro nome", 
+	   a.ultimo_nome as "Segundo nome",
+	   count (ac.curso_id) as "Número de cursos"
+from aluno a 
+join aluno_curso ac on ac.aluno_id = a.id 
+group by 1,2 
+order by "Número de cursos" desc  
+limit 1
+
+-- Limitando a três alunos
+select a.primeiro_nome as "Primeiro nome", 
+	   a.ultimo_nome as "Segundo nome",
+	   count (ac.curso_id) as "Número de cursos"
+from aluno a 
+join aluno_curso ac on ac.aluno_id = a.id 
+group by 1,2 
+order by "Número de cursos" desc  
+limit 3
+
+-- Limitando a três alunos a partir do registo 1
+select a.primeiro_nome as "Primeiro nome", 
+	   a.ultimo_nome as "Segundo nome",
+	   count (ac.curso_id) as "Número de cursos"
+from aluno a 
+join aluno_curso ac on ac.aluno_id = a.id 
+group by 1,2 
+order by "Número de cursos" desc  
+limit 3
+offset 1
+
+-- Pelos cursos mais requisitados (incluindo os que não tem alunos)
+select c.nome as "Nome do curso",
+	   count(ac.aluno_id) as "Numero de alunos"
+from curso c 
+left join aluno_curso ac on ac.curso_id = c.id 
+group by 1
+order by "Numero de alunos" desc 
+
+-- Pelo curso mais requisitado
+select c.nome as "Nome do curso",
+	   count(ac.aluno_id) as "Numero de alunos"
+from curso c 
+left join aluno_curso ac on ac.curso_id = c.id 
+group by 1
+order by "Numero de alunos" desc 
+limit 1
