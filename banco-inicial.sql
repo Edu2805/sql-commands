0c57234c492df4df298925e1f44eae3f15c395dc
Eduardo <eduardo@MacBook-Air-de-Eduardo.local>
2022-12-04 12:45:51 -0300

    Funções

--- banco-inicial.sql
+++ banco-inicial.sql
@@ -37,7 +37,7 @@ values ('Carlos', 'Alberto', '1980-10-11'),
 select * from aluno a 
 
 insert into categoria (nome) 
-values('Desenvolvimento'), 
+values('UX Designer'), ('Desenvolvimento'), 
 ('DevOps'), ('Data Science'), 
 ('Backend'), ('Frontend');
 
@@ -150,3 +150,93 @@ left join aluno_curso ac on ac.curso_id = c.id
 group by 1
 order by "Numero de alunos" desc 
 limit 1
+
+--- Operador IN
+-- busca tradicional
+select * from categoria c where c.id = 1 or c.id = 3
+
+-- busca com o IN
+select * from categoria c where c.id in (1,3)
+
+-- com subquery que não contenha espaços no nome
+
+select * from categoria c where c.nome not like '% %'
+select * from categoria c2 
+
+select * from curso c where c.categoria_id in (
+	select id from categoria cat where cat.nome not like '% %'
+);
+
+-- subquery para trazer somente a categoria de banco de dados
+
+select categoria
+	from (
+		select categoria.nome as categoria,
+			count(curso.id) as numero_cursos
+		from categoria
+		join curso on curso.categoria_id = categoria.id 
+		group by categoria
+	) as  categoria_cursos
+where numero_cursos >= 2
+
+-- poderia ser usado também
+select categoria.nome as categoria,
+	count(curso.id) as numero_cursos
+	from categoria
+	join curso on curso.categoria_id = categoria.id 
+group by categoria
+having count(curso.id) >= 2 
+
+--- Unir elemento na consulta
+
+select (primeiro_nome || ' ' || ultimo_nome) from aluno
+
+-- Lidando com o null quando queremos unir (concat)
+select concat('Eduardo', ' ', null, 'De', ' ', 'Amorim' ) as "Nome completo"
+
+-- Em letras maiúsculas
+select upper(concat('Eduardo', ' ', null, 'De', ' ', 'Amorim' )) as "Nome completo"
+
+-- Em letras minúsculas
+select lower(concat('Eduardo', ' ', null, 'De', ' ', 'Amorim' )) as "Nome completo"
+
+-- Remover espaços do início e do fim da função (trim)
+select trim(upper(concat('Eduardo', ' ', null, 'De', ' ', 'Amorim' ))) as "Nome completo"
+
+--- Calcular a idade dos alunos
+-- pegando a data atual, fazendo a formação para data usando (::)
+select (primeiro_nome || ' ' || ultimo_nome) as "Nome completo", now()::date , data_nascimento as "Data de nascimento" 
+from aluno
+
+-- Calculando a idade dos alunos
+select (primeiro_nome || ' ' || ultimo_nome) as "Nome completo", 
+		(now()::date - data_nascimento) / 365 as "Idade" 
+from aluno
+
+-- Simplificando, usando o AGE()
+select (primeiro_nome || ' ' || ultimo_nome) as "Nome completo", 
+		age(data_nascimento) as "Idade" 
+from aluno
+
+-- Extraindo somente o ano (extract(year from ...)
+select (primeiro_nome || ' ' || ultimo_nome) as "Nome completo", 
+		extract(year from age(data_nascimento)) as "Idade" 
+from aluno
+
+--- Funções de conversão de tipos
+-- Data 
+select now()::date
+
+-- converter data para DD/MM/YYYY
+select to_char(now(), 'DD/MM/YYYY') as "Data atual"
+
+-- Outras maneiras
+select to_char(now(), 'DAY, MONTH, YYYY') as "Data atual"
+select to_char(now(), 'DAY DD, MONTH MM, YYYY') as "Data atual"
+
+-- Números reais
+select 128.3
+select 128.3::real 
+
+-- Inserindo dois dígitos (transformando em string)
+select to_char(128.3::real, '999D99') 
